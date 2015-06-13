8c87210a83ed65b08081462ad53462162c8228a8
Michael Schnell <michael@fuin.org>
2015-06-13 10:11:37 +0200

    Support subscriptions

---
+++ api/src/main/java/org/fuin/esc/api/Subscription.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (C) 2015 Michael Schnell. All rights reserved. <http://www.fuin.org/>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.fuin.esc.api;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Result of subscribing to a stream. The sub classes will contain
+ * implementation specific data the is required to handle unsubscribe requests.
+ */
+public abstract class Subscription implements Serializable {
+
+    private static final long serialVersionUID = 1000L;
+
+    private StreamId streamId;
+
+    private Optional<Integer> lastEventNumber;
+
+    /**
+     * Constructor with all mandatory data.
+     * 
+     * @param streamId
+     *            Unique stream identifier.
+     * @param lastEventNumber
+     *            Number of the last event written to the stream.
+     */
+    public Subscription(@NotNull final StreamId streamId,
+            @NotNull final Optional<Integer> lastEventNumber) {
+        this.streamId = streamId;
+        this.lastEventNumber = lastEventNumber;
+    }
+
+    /**
+     * Returns the unique stream identifier.
+     * 
+     * @return Stream ID.
+     */
+    @NotNull
+    public final StreamId getStreamId() {
+        return streamId;
+    }
+
+    /**
+     * Returns the number of the last event written to the stream.
+     * 
+     * @return Event number.
+     */
+    @NotNull
+    public final Optional<Integer> getLastEventNumber() {
+        return lastEventNumber;
+    }
+
+}
