8f3e4c7dca890617fb2d242d295030b61ae6da81
Michael Schnell <michael@fuin.org>
2015-12-29 07:36:03 +0100

    Fixed missing equals/hashcode and tostring

--- test/src/test/java/org/fuin/esc/test/BookAddedEvent.java
+++ test/src/test/java/org/fuin/esc/test/BookAddedEvent.java
@@ -27,7 +27,7 @@ public class BookAddedEvent {
 
     /** Never changing unique event type name. */
     public static final String TYPE = "BookAddedEvent";
-    
+
     @XmlAttribute
     private String name;
 
@@ -44,8 +44,10 @@ public class BookAddedEvent {
     /**
      * Constructor with name and author.
      * 
-     * @param name Name.
-     * @param author Author.
+     * @param name
+     *            Name.
+     * @param author
+     *            Author.
      */
     public BookAddedEvent(final String name, final String author) {
         super();
@@ -65,6 +67,52 @@ public class BookAddedEvent {
      */
     public final String getAuthor() {
         return author;
+    }
+
+    // CHECKSTYLE:OFF Generated code
+    @Override
+    public final int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((author == null) ? 0 : author.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public final boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        BookAddedEvent other = (BookAddedEvent) obj;
+        if (author == null) {
+            if (other.author != null) {
+                return false;
+            }
+        } else if (!author.equals(other.author)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    // CHECKSTYLE:ON
+
+    @Override
+    public final String toString() {
+        return "BookAddedEvent [name=" + name + ", author=" + author + "]";
     }
 
 }
