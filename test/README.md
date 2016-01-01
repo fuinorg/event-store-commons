ca85834ca1c2540d041fb0e495a4c6597ca0f286
Michael Schnell <michael@fuin.org>
2016-01-01 20:14:12 +0100

    Added results

--- test/README.md
+++ test/README.md
@@ -5,11 +5,11 @@ Provides tests for all event store commons implementations.
 
 **No profiling was done yet** - So the below results only give a rough overview about the current state.
 
-| Implementation                                                                   | Type                | One append (100,000 at once) | Multiple appends (100,000 times) | 
-|:---------------------------------------------------------------------------------|:--------------------|:-----------------------------|:---------------------------------|
-| [esc-eshttp](src/test/java/org/fuin/esc/test/performance/EsHttpPerformance.java) | Physical PC SSD (1) | 2,467 events / second        |  96 events / second              |
-| [esc-eshttp](src/test/java/org/fuin/esc/test/performance/EsHttpPerformance.java) | Physical PC HDD (2) | 2,426 events / second        |  15 events / second              |
-| [esc-eshttp](src/test/java/org/fuin/esc/test/performance/EsHttpPerformance.java) | Virtual Machine (3) | 4,430 events / second        | 369 events / second              |
+| Implementation                                                                   | Type                | One append (100,000 at once) | Multiple appends (100,000 times) | Read chunks of 4,000 (100,000 events) | Read one by one (100,000 events) | 
+|:---------------------------------------------------------------------------------|:--------------------|:-----------------------------|:---------------------------------|:--------------------------------------|:---------------------------------|
+| [esc-eshttp](src/test/java/org/fuin/esc/test/performance/EsHttpPerformance.java) | Physical PC SSD (1) | 2,467 events / second        |  96 events / second              | 901 events / second                   | 464 events / second              |
+| [esc-eshttp](src/test/java/org/fuin/esc/test/performance/EsHttpPerformance.java) | Physical PC HDD (2) | 2,426 events / second        |  15 events / second              | 909 events / second                   | 459 events / second              |
+| [esc-eshttp](src/test/java/org/fuin/esc/test/performance/EsHttpPerformance.java) | Virtual Machine (3) | 4,042 events / second        | 379 events / second              | 832 events / second                   | 403 events / second              |
 
 * 64 Bit Java 8 Runtime
 * (1) Intel Core i7-3820 @ 3.60 GHz, 16 GB RAM, Windows 7 SP 1, Crucial M4 512GB SSD
--- test/src/test/java/org/fuin/esc/test/performance/EsHttpPerformance.java
+++ test/src/test/java/org/fuin/esc/test/performance/EsHttpPerformance.java
@@ -33,6 +33,7 @@ import org.fuin.esc.api.EventStoreSync;
 import org.fuin.esc.api.ExpectedVersion;
 import org.fuin.esc.api.SimpleCommonEvent;
 import org.fuin.esc.api.SimpleStreamId;
+import org.fuin.esc.api.StreamEventsSlice;
 import org.fuin.esc.api.StreamId;
 import org.fuin.esc.api.TypeName;
 import org.fuin.esc.eshttp.ESEnvelopeType;
@@ -60,7 +61,7 @@ public final class EsHttpPerformance {
 
         waitForInput("START");
 
-        final int max = 10000;
+        final int max = 100000;
 
         final ThreadFactory threadFactory = Executors.defaultThreadFactory();
         final URL url = new URL("http://127.0.0.1:2113/");
@@ -85,7 +86,7 @@ public final class EsHttpPerformance {
                     .add("author", "Stephen King").build();
             final JsonObject meta = Json.createObjectBuilder().add("user", "michael").build();
 
-            waitForInput("TEST 1");
+            waitForInput("APPEND 1");
 
             // Append all together
             final StreamId streamId1 = new SimpleStreamId("books1", false);
@@ -96,7 +97,7 @@ public final class EsHttpPerformance {
             measure("One append (" + togetherList.size() + " events)", togetherList.size(),
                     none -> eventStore.appendToStream(streamId1, ExpectedVersion.ANY.getNo(), togetherList));
 
-            waitForInput("TEST 2");
+            waitForInput("APPEND 2");
 
             // Append all separate
             final StreamId streamId2 = new SimpleStreamId("books2", false);
@@ -110,6 +111,41 @@ public final class EsHttpPerformance {
                 }
             });
 
+            waitForInput("READ 1");
+
+            // Read big chunks
+            final int chunkSize = 4000;
+            measure("Read chunks of " + chunkSize + " (" + max + " events)", max, name -> {
+                int start = 0;
+                int count = 0;
+                StreamEventsSlice slice;
+                do {
+                    slice = eventStore.readEventsForward(streamId1, start, chunkSize);
+                    start = slice.getNextEventNumber();
+                    count = count + slice.getEvents().size();
+                } while (!slice.isEndOfStream());
+                System.out.println("NO OF EVENTS   " + name + ": " + count);
+            });
+
+            waitForInput("READ 2");
+
+            // Read single
+            measure("Read one by one (" + max + " events)", max, name -> {
+                int count = 0;
+                int start = 0;
+                StreamEventsSlice slice;
+                do {
+                    slice = eventStore.readEventsForward(streamId2, start, 1);
+                    start = slice.getNextEventNumber();
+                    count = count + slice.getEvents().size();
+                    if (count % 1000 == 0) {
+                        System.out.print(".");
+                    }
+                } while (!slice.isEndOfStream());
+                System.out.println();
+                System.out.println("NO OF EVENTS   " + name + ": " + count);
+            });
+
         } finally {
             eventStore.close();
         }
@@ -118,10 +154,10 @@ public final class EsHttpPerformance {
 
     }
 
-    private static void measure(final String name, final int count, final Consumer<Void> func) {
+    private static void measure(final String name, final int count, final Consumer<String> func) {
         final long start = System.currentTimeMillis();
         System.out.println("START          " + name + ": " + start);
-        func.accept(null);
+        func.accept(name);
         final long end = System.currentTimeMillis();
         System.out.println("END            " + name + ": " + end);
         final double millis = end - start;
