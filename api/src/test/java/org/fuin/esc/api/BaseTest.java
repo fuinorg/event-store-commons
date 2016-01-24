1c20e4ca95c10d6bc5f37746d1b1c22764d33ab5
Michael Schnell <michael@fuin.org>
2016-01-24 11:11:19 +0100

    Added new test

--- api/src/test/java/org/fuin/esc/api/BaseTest.java
+++ api/src/test/java/org/fuin/esc/api/BaseTest.java
@@ -17,9 +17,15 @@
  */
 package org.fuin.esc.api;
 
+import static org.fuin.units4j.JandexAssert.assertThat;
+
 import java.io.File;
+import java.util.List;
 
 import org.fuin.units4j.AssertCoverage;
+import org.fuin.units4j.Units4JUtils;
+import org.jboss.jandex.Index;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -32,6 +38,20 @@ public class BaseTest {
     public final void testCoverage() {
         // Make sure all classes have a test
         AssertCoverage.assertEveryClassHasATest(new File("src/main/java"));
+    }
+
+    @Ignore("TODO Fix failing test with new units4j version")
+    @Test
+    public final void testNullability() {
+
+        // Collect all class files
+        File dir = new File("target/classes");
+        List<File> classFiles = Units4JUtils.findAllClasses(dir);
+        Index index = Units4JUtils.indexAllClasses(classFiles);
+
+        // Verify that all methods make a statement if null is allowed or not
+        assertThat(index).hasNullabilityInfoOnAllMethods();
+
     }
 
 }
