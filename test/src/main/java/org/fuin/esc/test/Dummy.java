acccec6396324ca6b43dbba85bcb70f46df2183e
Michael Schnell <michael@fuin.org>
2016-09-04 07:07:10 +0200

    Added class to fix sonatype build

---
+++ test/src/main/java/org/fuin/esc/test/Dummy.java
@@ -0,0 +1,12 @@
+//CHECKSTYLE:OFF
+package org.fuin.esc.test;
+
+/**
+* Circumvents Maven build problem with empty main jar with OSS Sonatype deployment. 
+* 
+* TODO Investigate how to configure Maven for a Sontype build
+*/
+public class Dummy {
+
+}
+//CHECKSTYLE:ON
