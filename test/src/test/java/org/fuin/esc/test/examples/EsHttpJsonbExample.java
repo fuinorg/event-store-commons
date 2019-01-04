fe974f939a469ff69c71db280d06bdcdb5d56400
Michael Schnell <michael@fuin.org>
2019-01-04 11:17:57 +0100

    Fixed JSON-B example

--- test/src/test/java/org/fuin/esc/test/examples/EsHttpJsonbExample.java
+++ test/src/test/java/org/fuin/esc/test/examples/EsHttpJsonbExample.java
@@ -7,8 +7,6 @@ import java.nio.charset.Charset;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
-import javax.json.Json;
-import javax.json.JsonObject;
 import javax.json.bind.JsonbConfig;
 
 import org.eclipse.yasson.FieldAccessStrategy;
@@ -19,13 +17,15 @@ import org.fuin.esc.api.ExpectedVersion;
 import org.fuin.esc.api.SimpleCommonEvent;
 import org.fuin.esc.api.SimpleStreamId;
 import org.fuin.esc.api.StreamId;
-import org.fuin.esc.api.TypeName;
 import org.fuin.esc.eshttp.ESEnvelopeType;
 import org.fuin.esc.eshttp.ESHttpEventStore;
 import org.fuin.esc.spi.EscEvent;
 import org.fuin.esc.spi.EscEvents;
 import org.fuin.esc.spi.EscMeta;
+import org.fuin.esc.spi.EscSpiUtils;
 import org.fuin.esc.spi.JsonbDeSerializer;
+import org.fuin.esc.spi.SerDeserializerRegistry;
+import org.fuin.esc.spi.SerializedDataTypeRegistry;
 import org.fuin.esc.spi.SimpleSerializedDataTypeRegistry;
 import org.fuin.esc.spi.SimpleSerializerDeserializerRegistry;
 
@@ -37,18 +37,8 @@ public final class EsHttpJsonbExample {
     private EsHttpJsonbExample() {
         super();
     }
-
-    /**
-     * Main method.
-     * 
-     * @param args
-     *            Not used.
-     */
-    public static void main(final String[] args) throws MalformedURLException {
-
-        // Setup for
-        ThreadFactory threadFactory = Executors.defaultThreadFactory();
-        URL url = new URL("http://127.0.0.1:2113/"); // Default event store port
+    
+    private static SerializedDataTypeRegistry createTypeRegistry() {
 
         // Contains all types for usage with JSON-B
         SimpleSerializedDataTypeRegistry typeRegistry = new SimpleSerializedDataTypeRegistry();
@@ -61,10 +51,18 @@ public final class EsHttpJsonbExample {
         typeRegistry.add(MyMeta.SER_TYPE, MyMeta.class);
         typeRegistry.add(BookAddedEvent.SER_TYPE, BookAddedEvent.class);
         
-        JsonbConfig config = new JsonbConfig().withPropertyVisibilityStrategy(new FieldAccessStrategy());
-        JsonbDeSerializer jsonbDeSer = new JsonbDeSerializer(config, Charset.forName("utf-8"), typeRegistry);
+        return typeRegistry;
+    }
 
-        // Registry connects the type with the appropriate serializer and de-serializer
+    /**
+     * Creates a registry that connects the type with the appropriate serializer and de-serializer.
+     * 
+     * @param jsonbDeSer JSON-B serializer/deserializer to use.
+     * 
+     * @return New registry instance.
+     */
+    private static SerDeserializerRegistry createSerDeserializerRegistry(JsonbDeSerializer jsonbDeSer) {
+        
         SimpleSerializerDeserializerRegistry registry = new SimpleSerializerDeserializerRegistry();
 
         // Base types always needed
@@ -75,12 +73,45 @@ public final class EsHttpJsonbExample {
         // User defined types
         registry.add(MyMeta.SER_TYPE, "application/json", jsonbDeSer);
         registry.add(BookAddedEvent.SER_TYPE, "application/json", jsonbDeSer);
+        
+        return registry;
+    }
+    
+    private static JsonbDeSerializer createJsonbDeSerializer(SerializedDataTypeRegistry typeRegistry) {
+        JsonbConfig config = new JsonbConfig()
+                .withSerializers(EscSpiUtils.createEscJsonbSerializers(typeRegistry))
+                .withDeserializers(EscSpiUtils.createEscJsonbDeserializers(typeRegistry))
+                .withPropertyVisibilityStrategy(new FieldAccessStrategy());
+        return new JsonbDeSerializer(config, Charset.forName("utf-8"), typeRegistry);
+    }
+    
+    /**
+     * Main method.
+     * 
+     * @param args
+     *            Not used.
+     */
+    public static void main(final String[] args) throws MalformedURLException {
 
+        // Setup for local host connection to the event store
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        URL url = new URL("http://127.0.0.1:2113/"); // Default event store port
+
+        // Knows about all types for usage with JSON-B
+        SerializedDataTypeRegistry typeRegistry = createTypeRegistry();
+
+        // Does the actual marshalling/unmarshalling
+        JsonbDeSerializer jsonbDeSer = createJsonbDeSerializer(typeRegistry);
+        
+        // Registry connects the type with the appropriate serializer and de-serializer
+        SerDeserializerRegistry serDeserRegistry = createSerDeserializerRegistry(jsonbDeSer);
+
+        
         // Create an event store instance and open it
         EventStore eventStore = new ESHttpEventStore(threadFactory, url, 
                 ESEnvelopeType.JSON, // This format will be used to communicate with the event store
-                registry, // Registry used to find a serializer
-                registry // Registry used to find a de-serializer
+                serDeserRegistry, // Registry used to find a serializer
+                serDeserRegistry // Registry used to find a de-serializer
         );
         eventStore.open();
         try {
