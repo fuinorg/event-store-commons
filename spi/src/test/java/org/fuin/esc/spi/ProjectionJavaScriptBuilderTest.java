09b4fae6674d7a53da4a88fca8f24c2e425de3cc
Michael Schnell <1856319+michael-schnell@users.noreply.github.com>
2023-12-25 10:02:17 +0100

    Migrated to Junit5 (#20)

--- spi/src/test/java/org/fuin/esc/spi/ProjectionJavaScriptBuilderTest.java
+++ spi/src/test/java/org/fuin/esc/spi/ProjectionJavaScriptBuilderTest.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library. If not, see http://www.gnu.org/licenses/.
  */
-package org.fuin.esc.spi;
+package org.fuin.esc.eshttp;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -25,7 +25,6 @@ import java.util.List;
 
 import org.fuin.esc.api.SimpleStreamId;
 import org.fuin.esc.api.TypeName;
-import org.fuin.esc.spi.ProjectionJavaScriptBuilder;
 import org.junit.jupiter.api.Test;
 
 /**
