a7965bcddf057c02d30b96cc747f76921d3d8f23
Michael Schnell <michael@fuin.org>
2024-01-20 16:38:20 +0100

    Added new static utility method

--- api/src/test/java/org/fuin/esc/api/HasSerializedDataTypeConstantValidatorTest.java
+++ api/src/test/java/org/fuin/esc/api/HasSerializedDataTypeConstantValidatorTest.java
@@ -27,9 +27,11 @@ import org.junit.jupiter.api.Test;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public final class HasSerializedDataTypeConstantValidatorTest {
 
+    public static final String FIELD_NAME = "SER_TYPE";
     private static Validator validator;
 
     @BeforeAll
@@ -42,36 +44,80 @@ public final class HasSerializedDataTypeConstantValidatorTest {
     @Test
     public final void testValid() {
         assertThat(validator.validate(new MyClassValid())).isEmpty();
+        assertThat(HasSerializedDataTypeConstantValidator
+                .extractValue(MyClassValid.class, FIELD_NAME)).isEqualTo(new SerializedDataType("XYZ"));
     }
 
     @Test
     public final void testNotStatic() {
+
         assertThat(first(validator.validate(new MyClassNotStatic()))).contains("#1");
+
+        assertThatThrownBy(
+                () -> HasSerializedDataTypeConstantValidator.extractValue(MyClassNotStatic.class, FIELD_NAME))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("#1");
+
     }
 
     @Test
     public final void testNotPublic() {
+
         assertThat(first(validator.validate(new MyClassNotPublic()))).contains("#2");
+
+        assertThatThrownBy(
+                () -> HasSerializedDataTypeConstantValidator.extractValue(MyClassNotPublic.class, FIELD_NAME))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("#2");
+
     }
 
     @Test
     public final void testWrongReturnType() {
+
         assertThat(first(validator.validate(new MyClassWrongType()))).contains("#3");
+
+        assertThatThrownBy(
+                () -> HasSerializedDataTypeConstantValidator.extractValue(MyClassWrongType.class, FIELD_NAME))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("#3");
+
     }
 
     @Test
     public final void testWrongReturn() {
+
         assertThat(first(validator.validate(new MyClassNullValue()))).contains("#4");
+
+        assertThatThrownBy(
+                () -> HasSerializedDataTypeConstantValidator.extractValue(MyClassNullValue.class, FIELD_NAME))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("#4");
+
     }
 
     @Test
-    public final void testNoMethod() {
+    public final void testNoField() {
+
         assertThat(first(validator.validate(new MyClassNoField()))).contains("#2");
+
+        assertThatThrownBy(
+                () -> HasSerializedDataTypeConstantValidator.extractValue(MyClassNoField.class, FIELD_NAME))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("#2");
+
     }
 
     @Test
     public final void testNotFinal() {
+
         assertThat(first(validator.validate(new MyClassNotFinal()))).contains("#5");
+
+        assertThatThrownBy(
+                () -> HasSerializedDataTypeConstantValidator.extractValue(MyClassNotFinal.class, FIELD_NAME))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("#5");
+
     }
 
     private static String first(Set<?> violations) {
